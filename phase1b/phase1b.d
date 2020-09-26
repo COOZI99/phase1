02f782469e47719ed3c03336a53f8927db5acff3
Bianca Ana Lara <balara@cs.arizona.edu>
2020-09-26 11:51:04 -0700

    adding test files

---
+++ phase1b/phase1b.d
@@ -0,0 +1,2 @@
+phase1b.o: phase1b.c ../phase1Int.h ../phase1.h \
+ /home/balara/include/usloss.h /home/balara/include/usyscall.h
---
+++ phase1b/tests/test_fork.d
@@ -0,0 +1,2 @@
+test_fork.o: tests/test_fork.c ../phase1.h /home/balara/include/usloss.h \
+ /home/balara/include/usyscall.h ../phase1Int.h ../phase1.h
